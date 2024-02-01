dcd3a8cb270131dba4ecd97697dc753ebe0f8de8
dpfls0922 <dpfls0922@naver.com>
2024-02-01 23:10:46 +0900

    [Add] #5 로그아웃 url 추가

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 #users/urls.py
 from django.urls import path
-from .views import SignUpView, LoginView
+from .views import SignUpView, LoginView, LogoutView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
