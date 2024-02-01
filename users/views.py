d6b0a7d7211b3619105d79e97b9d27f9e6174f55
dpfls0922 <dpfls0922@naver.com>
2024-02-01 19:22:58 +0900

    [Feat] #5 로그인 뷰 구현

--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
 #users/views.py
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from .serializers import SignUpSerializer
+from .serializers import SignUpSerializer, LoginSerializer
 
 class SignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignUpSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        token = serializer.validated_data # validate()의 리턴값인 token 받아오기
+        return Response({"token": token.key}, status = status.HTTP_200_OK)
