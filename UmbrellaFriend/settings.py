afe23757899ab7203d2ea0e3a5dd87a4327a1d06
dpfls0922 <dpfls0922@naver.com>
2024-02-01 18:50:00 +0900

    [Add] #3 토큰 인증 방식으로 설정

--- UmbrellaFriend/settings.py
+++ UmbrellaFriend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'users',
 ]
 
@@ -112,6 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
