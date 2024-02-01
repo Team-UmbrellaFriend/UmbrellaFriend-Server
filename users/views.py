e5731b9f0570e8602807c6d895a0fde7ff352ac8
dpfls0922 <dpfls0922@naver.com>
2024-02-01 18:47:43 +0900

    [Feat] #3 generics의 CreateAPIView를 사용해 회원가입 기능 작성

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+#users/views.py
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import SignUpSerializer
+
+class SignUpView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
