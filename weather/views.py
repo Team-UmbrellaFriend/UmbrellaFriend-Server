ac6f3abbafed3ff0bda002c46d78e54c29bbb151
dpfls0922 <dpfls0922@naver.com>
2024-02-07 20:39:57 +0900

    [Fix] #9 key 제거

--- weather/views.py
+++ weather/views.py
@@ -1,16 +1,30 @@
 #weather/views.py
-from urllib.parse import urlencode, unquote
+from urllib.parse import urlencode
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import requests, json
+
+
+def load_api_key(filename="../secrets.json"):
+    try:
+        with open(filename, "r") as file:
+            secrets = json.load(file)
+            return secrets.get("WEATHER_KEY")
+    except FileNotFoundError:
+        print(f"{filename} 파일을 찾을 수 없습니다.")
+        return None
+    except json.JSONDecodeError:
+        print(f"{filename} 파일을 파싱할 수 없습니다.")
+        return None
+
 
 @api_view(['GET'])
 def get_rain_percent(request):
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
     queryString = "?" + urlencode(
         {
-        "serviceKey": unquote("GajU3wBEQyXaoPxusoceynnphlBgHHAZCBIakU0o85ex4aPwIWocpTYVKb%2BRBerkKLPjsRHwLOVyGmQ%2F10QeJQ%3D%3D"),
+        "serviceKey": load_api_key(),
         "pageNo": 1,
         "numOfRows": 10,
         "dataType": "JSON",
