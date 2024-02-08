2cb3ca4048b521d7cf11346e2a30a43143e6b6b7
dpfls0922 <dpfls0922@naver.com>
2024-02-08 14:49:23 +0900

    [Add] #11 우산에 장소 정보 추가

--- umbrella/models.py
+++ umbrella/models.py
@@ -5,5 +5,18 @@ class Umbrella(models.Model):
     number = models.IntegerField(unique = True)
     is_available = models.BooleanField(default = True)
 
+    place = [
+        ('명신관', '명신관'),
+        ('순헌관', '순헌관'),
+        ('학생회관', '학생회관'),
+        ('도서관', '도서관')
+    ]
+    
+    location = models.CharField(
+        max_length = 20,
+        choices = place,
+        default = '도서관'
+    )
+
     def __str__(self):
         return f'[{self.number}] Umbrella'
