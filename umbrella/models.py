c23325e342fdaab42011f37cda93e5204039d74b
dpfls0922 <dpfls0922@naver.com>
2024-02-08 23:14:25 +0900

    [Add] #11 대여 및 반납 정보를 담는 Rent 모델 생성

--- umbrella/models.py
+++ umbrella/models.py
@@ -1,5 +1,8 @@
 #umbrella/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Umbrella(models.Model):
     number = models.IntegerField(unique = True)
@@ -20,3 +23,17 @@ class Umbrella(models.Model):
 
     def __str__(self):
         return f'[{self.number}] Umbrella'
+
+
+class Rent(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    umbrella = models.ForeignKey(Umbrella, on_delete = models.CASCADE)
+    rent_date = models.DateTimeField(default = timezone.now) # 대여 날짜
+    return_date = models.DateTimeField(null = True, blank = True) # 반납 날짜
+    return_due_date = models.DateTimeField(default = timezone.now() + timezone.timedelta(days = 3)) # 반납 기한
+
+    def is_overdue(self):
+        return timezone.now() > self.return_due_date
+
+    def __str__(self):
+        return f'[{self.user}] {self.umbrella}'
