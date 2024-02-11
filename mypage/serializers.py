42453eb80d492b74d6a3558490db32d83a48be2a
dpfls0922 <dpfls0922@naver.com>
2024-02-11 21:45:47 +0900

    [Feat] #20 마이페이지 관련 시리얼라이저 구현

---
+++ mypage/serializers.py
@@ -0,0 +1,24 @@
+#mypage/serializers.py
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from users.models import Profile
+from umbrella.models import Rent
+
+class MyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class MyProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('studentID', 'phoneNumber')
+
+
+class MyRentSerializer(serializers.ModelSerializer):
+    rent_date = serializers.DateTimeField(format = "%Y년%m월%d일", read_only=True)
+    rental_period = serializers.CharField(max_length=10, read_only=True)
+
+    class Meta:
+        model = Rent
+        fields = ['rental_period', 'rent_date']
