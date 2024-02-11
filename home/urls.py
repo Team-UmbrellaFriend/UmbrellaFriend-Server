34629d273b34c175b3f90d47daafd0edc031aade
dpfls0922 <dpfls0922@naver.com>
2024-02-11 23:41:49 +0900

    [Chore] #20 접근 url 수정

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-    path('info/', HomeView.as_view()),
+    path('', HomeView.as_view()),
 ]
--- mypage/urls.py
+++ mypage/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import MyPageView
 
 urlpatterns = [
-    path('info/', MyPageView.as_view()),
+    path('', MyPageView.as_view()),
 ]
