7447c9ae05a6d46d8b1386101a9fb27814602cd0
dpfls0922 <dpfls0922@naver.com>
2024-02-11 23:33:11 +0900

    [Chore] #20 회원가입 시 모바일 학생증 저장 위치 변경

--- users/models.py
+++ users/models.py
@@ -2,23 +2,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from umbrella.models import Umbrella
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
 
 class Profile(models.Model):
-    
-    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True, related_name='profile')
-    # primary_key를 User의 pk로 설정하여 통합적으로 관리
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True, related_name = 'profile')
     studentID = models.IntegerField(default = 0, unique = True)
-    studentCard = models.ImageField(upload_to = 'media/')
+    studentCard = models.ImageField(upload_to = f'profile')
     phoneNumber = models.CharField(max_length = 20)
     umbrella = models.OneToOneField(Umbrella, related_name = 'user', null = True, blank = True, on_delete = models.SET_NULL)
-
-    # @receiver(post_save, sender = User)
-    # def create_student_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         Profile.objects.create(user = instance)
-
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.profile.save()
