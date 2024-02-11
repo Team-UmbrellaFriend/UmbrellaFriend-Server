b2b3311a4b456b5b350bc2296ac155dd57af26ba
dpfls0922 <dpfls0922@naver.com>
2024-02-11 22:10:02 +0900

    [Del] #20 사용되지 않는 임포트문 제거

--- umbrella/views.py
+++ umbrella/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Umbrella, Rent
-from .serializers import UmbrellaSerializer, RentSerializer
+from .serializers import RentSerializer
 from datetime import *
 from django.utils import timezone
 from django.db.models import Count
