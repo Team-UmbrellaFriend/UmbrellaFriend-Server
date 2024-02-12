b15debd452af6cb49388c9f04c8bdd1711991fc0
dpfls0922 <dpfls0922@naver.com>
2024-02-12 17:00:47 +0900

    [Del] #20 사용하지 않는 시리얼라이저 제거

--- umbrella/serializers.py
+++ umbrella/serializers.py
@@ -4,12 +4,6 @@ from .models import Umbrella, Rent
 from django.contrib.auth.models import User
 
 
-class UmbrellaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Umbrella
-        fields = '__all__'
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
