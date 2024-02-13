13b716658b7a0c433dc226fcddd558d43287ae15
dpfls0922 <dpfls0922@naver.com>
2024-02-14 01:06:31 +0900

    [Add] #21 시리얼라이저에 이미지 추가

--- umbrella/serializers.py
+++ umbrella/serializers.py
@@ -11,9 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RentSerializer(serializers.ModelSerializer):
-    rent_date = serializers.DateTimeField(format = "%Y년%m월%d일", read_only=True)
-    rental_period = serializers.CharField(max_length=10, read_only=True)
+    rent_date = serializers.DateTimeField(format = "%Y년%m월%d일", read_only = True)
+    rental_period = serializers.CharField(max_length = 10, read_only = True)
+    image = serializers.ImageField(required = False, allow_empty_file = True)
 
     class Meta:
         model = Rent
-        fields = ['user', 'umbrella', 'rental_period', 'rent_date']
+        fields = ['user', 'umbrella', 'rental_period', 'rent_date', 'image']
