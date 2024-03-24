def2538024d72756ebfe6b7781e65cc6d7091fdb
dpfls0922 <dpfls0922@naver.com>
2024-03-24 16:21:29 +0900

    [Chore] 회원가입 및 프로필 수정 오류 발생 시 응답 데이터 형식 변경

--- users/views.py
+++ users/views.py
@@ -15,7 +15,43 @@ class SignUpView(generics.CreateAPIView):
         if serializer.is_valid():
             user_data = serializer.save()
             return Response({'status': status.HTTP_201_CREATED, 'message': '응답 성공', 'data': {'token': user_data['token']}}, status = status.HTTP_201_CREATED)
-        return Response({'status': status.HTTP_400_BAD_REQUEST, 'message': '응답 실패', 'data': serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+        else:
+            errors = serializer.errors
+            error_messages = {
+                "profile_studentID": "이미 존재하는 학생 ID입니다.",
+                "email": "이미 등록된 이메일 주소입니다."
+            }
+            profile_error = None
+            email_error = None
+            password_error = None
+
+            for field, error in errors.items():
+                if field == 'profile':
+                    if not profile_error:
+                        for sub_field, sub_error in error.items():
+                            if sub_field == 'studentID':
+                                profile_error = error_messages.get("profile_studentID")
+                elif field == 'email':
+                    if not email_error:
+                        email_error = error_messages.get("email")
+                elif field == 'password':
+                    if not password_error:
+                        password_errors = []
+                        for error_detail in error:
+                            if error_detail.code == 'blank':
+                                password_errors.append("비밀번호를 입력하세요.")
+                            elif error_detail.code == 'password_too_short':
+                                password_errors.append("비밀번호는 최소 8자 이상이어야 합니다.")
+                            elif error_detail.code == 'invalid':
+                                password_errors.append("비밀번호가 일치하지 않습니다.")
+                        password_error = password_errors
+            if profile_error:
+                return Response({'status': 400, 'message': profile_error, 'data': ''}, status = status.HTTP_400_BAD_REQUEST)
+            if email_error:
+                return Response({'status': 400, 'message': email_error, 'data': ''}, status = status.HTTP_400_BAD_REQUEST)
+            if password_error:
+                return Response({'status': 400, 'message': password_error[0], 'data': ''}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({'status': 400, 'message': '유효하지 않은 데이터입니다.', 'data': ''}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(generics.GenericAPIView):
@@ -50,4 +86,29 @@ class ProfileView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({'status': status.HTTP_200_OK, 'message': '응답 성공', 'data': serializer.data}, status = status.HTTP_200_OK)
-        return Response({'status': status.HTTP_400_BAD_REQUEST, 'message': '응답 실패', 'data': serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+        else:
+            errors = serializer.errors
+            invalid_error = None
+            password_error = None
+
+            for field, error in errors.items():
+                if field == 'password':
+                    if not password_error:
+                        password_errors = []
+                        for error_detail in error:
+                            if error_detail.code == 'password_too_short':
+                                password_errors.append("비밀번호는 최소 8자 이상이어야 합니다.")
+                            elif error_detail.code == 'password_too_common':
+                                password_errors.append("비밀번호는 최소 8자 이상이어야 합니다.")
+                            elif error_detail.code == 'invalid':
+                                password_errors.append("비밀번호가 일치하지 않습니다.")
+                        password_error = password_errors
+                elif field == 'non_field_errors':
+                    if not invalid_error:
+                        invalid_error = error
+
+            if invalid_error:
+                return Response({'status': 400, 'message': invalid_error[0], 'data': ''}, status = status.HTTP_400_BAD_REQUEST)
+            if password_error:
+                return Response({'status': 400, 'message': password_error[0], 'data': ''}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({'status': 400, 'message': '유효하지 않은 데이터입니다.', 'data': ''}, status=status.HTTP_400_BAD_REQUEST)
