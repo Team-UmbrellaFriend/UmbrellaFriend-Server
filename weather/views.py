7f2b1d85d475689af70058563aa1a1af36130d1b
dpfls0922 <dpfls0922@naver.com>
2024-04-12 00:20:31 +0900

    [Feat] #31 강수 확률 불러오는 뷰 리팩토링 및 캐시값 설정

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,17 @@
 #weather/views.py
 from urllib.parse import urlencode
+from django.core.cache import cache
 from datetime import datetime, timedelta
 import requests, json
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def load_api_key(filename="secrets.json"):
+    """
+    API 키를 파일에서 로드하는 함수
+    """
     try:
         with open(filename, "r") as file:
             secrets = json.load(file)
@@ -18,6 +25,9 @@ def load_api_key(filename="secrets.json"):
 
 
 def calculate_base_date_and_time():
+    """
+    기준 날짜와 시간을 계산하는 함수
+    """
     current_time = datetime.now()
     base_times = ["0200", "0500", "0800", "1100", "1400", "1700", "2000", "2300"]
 
@@ -33,27 +43,33 @@ def calculate_base_date_and_time():
     return current_time, base_date, base_time
 
 
-def get_rain_percent(request):
-    current_time, base_date, base_time = calculate_base_date_and_time()
-
+def fetch_rain_data(base_date, base_time):
+    """
+    외부 API를 통해 강수 정보를 가져오는 함수
+    """
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
     queryString = "?" + urlencode(
         {
-        "serviceKey": load_api_key(),
-        "pageNo": 1,
-        "numOfRows": 10,
-        "dataType": "JSON",
-        "base_date": base_date,
-        "base_time": base_time,
-        "nx": 60,   # 청파동 위치 좌표 (nx, ny)
-        "ny": 125,
+            "serviceKey": load_api_key(),
+            "pageNo": 1,
+            "numOfRows": 10,
+            "dataType": "JSON",
+            "base_date": base_date,
+            "base_time": base_time,
+            "nx": 60,  # 청파동 위치 좌표 (nx, ny)
+            "ny": 125,
         }
     )
     queryURL = url + queryString
     response = requests.get(queryURL)
 
-    date = current_time.strftime("%Y") + "년 " + current_time.strftime("%m") + "월 " + current_time.strftime("%d") + "일"
+    return response
 
+
+def parse_rain_data(response):
+    """
+    API 응답에서 강수 정보를 파싱하는 함수
+    """
     try:
         r_dict = json.loads(response.text)
         r_response = r_dict.get("response", {})
@@ -61,15 +77,57 @@ def get_rain_percent(request):
         r_items = r_body.get("items", {})
         r_item = r_items.get("item", None)
 
-        result = {}
         if r_item:
             for item in r_item:
-                if(item.get("category") == "POP"): # 강수 확률
-                    result = item
-                    break
-        if isinstance(result, dict) and "fcstValue" in result:
-            return {"date": date, "percent": result["fcstValue"]}
-        else:
-            return {'date': date, 'percent': "-1"}
+                if item.get("category") == "POP":  # 강수 확률
+                    return item.get("fcstValue")
     except json.JSONDecodeError:
-        return {'date': date, 'percent': "-1"}
+        pass
+
+    return "0"  # 기본값 반환
+
+
+def calculate_next_base_time(current_time, base_time):
+    """
+    다음 기준 시간을 계산하는 함수
+    """
+    next_hour = (int(base_time) + 300) % 2400
+    next_base_time = f"{next_hour:04d}"
+    
+    if next_base_time == "0200" and base_time == "2300":
+        # 현재 시간이 2300이고, 다음 시간이 0200인 경우 다음 날의 시간을 계산
+        next_day = current_time + timedelta(days=1)
+        return datetime.combine(next_day, datetime.strptime(next_base_time, '%H%M').time())
+    else:
+        return datetime.combine(datetime.today(), datetime.strptime(next_base_time, '%H%M').time())
+
+
+def get_rain_percent(request):
+    """
+    강수 확률을 반환하고 캐시에 저장하는 함수
+    """
+    current_time, base_date, base_time = calculate_base_date_and_time()
+
+    # 캐시가 존재하면 캐시 값 리턴
+    cache_key = f'rain_percent_{base_date}_{base_time}'
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        logger.info(f"Caching data: {cached_data}")
+        return cached_data
+
+    # 캐시가 존재하지 않을 때 외부 API 호출해서 강수 확률 가져오기
+    response = fetch_rain_data(base_date, base_time)
+    rain_percent = parse_rain_data(response)
+
+    date = current_time.strftime("%Y") + "년 " + current_time.strftime("%m") + "월 " + current_time.strftime("%d") + "일"
+    rain_data = {"date": date, "percent": rain_percent}
+
+    # 캐시에 강수 확률 저장
+    next_base_time = calculate_next_base_time(current_time, base_time)
+    remaining_time = next_base_time - current_time
+    
+    cache_timeout = remaining_time.total_seconds()
+    cache.set(cache_key, rain_data, timeout=cache_timeout)
+
+    logger.info(f"Caching data for key: {cache_key}, data: {rain_data}, current_time: {current_time}, next_time: {next_base_time}, remaining_time: {remaining_time}")
+    return rain_data
