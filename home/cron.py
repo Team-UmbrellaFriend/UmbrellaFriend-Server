c0e514b27db3e2edf348d72295ca8001d3036927
dpfls0922 <dpfls0922@naver.com>
2024-05-12 23:58:10 +0900

    [Add] #47 7일이 지난 회원탈퇴 기록 삭제를 crontab으로 관리

--- home/cron.py
+++ home/cron.py
@@ -1,4 +1,7 @@
 from django.core.management import call_command
 
-def send_return_reminder_email():
+def send_return_reminders():
     call_command('send_return_reminders')
+
+def delete_withdrawal_records():
+    call_command('delete_expired_withdrawal_records')
