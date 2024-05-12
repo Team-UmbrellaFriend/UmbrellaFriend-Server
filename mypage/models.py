d49a044334a4bdbf6fb921c7ca79c8e87a4aa9ec
dpfls0922 <dpfls0922@naver.com>
2024-05-12 23:38:19 +0900

    [Chore] #47 탈퇴하려는 회원이 신고한 데이터는 남겨두도록 로직 수정

--- mypage/models.py
+++ mypage/models.py
@@ -12,13 +12,19 @@ class UmbrellaReport(models.Model):
     )
 
     umbrella = models.ForeignKey(Umbrella, on_delete = models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null = True)
     report_reason = models.CharField(max_length = 10, choices = REPORT_CHOICES)
     description = models.TextField(max_length = 200, blank = True, null = True)
     is_done = models.BooleanField(default = False)
 
     def __str__(self):
-        if self.is_done:
-            return f'[{self.user.username}] 우산{self.umbrella.number} {self.report_reason} - 해결완료(O)'
+        if self.user:
+            if self.is_done:
+                return f'[{self.user.username}] 우산{self.umbrella.number} {self.report_reason} - 해결완료(O)'
+            else:
+                return f'[{self.user.username}] 우산{self.umbrella.number} {self.report_reason} - 해결안됨(X)'
         else:
-            return f'[{self.user.username}] 우산{self.umbrella.number} {self.report_reason} - 해결안됨(X)'
+            if self.is_done:
+                return f'[탈퇴한 회원] 우산{self.umbrella.number} {self.report_reason} - 해결완료(O)'
+            else:
+                return f'[탈퇴한 회원] 우산{self.umbrella.number} {self.report_reason} - 해결안됨(X)'
